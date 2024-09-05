ef3219b232610845a2d7de534eaa4d9dfbf26dcf
hdircksen <106742695+hdircksen@users.noreply.github.com>
2024-09-05 17:03:57 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,135 @@
+export const perguntas = [
+    {
+        enunciado: "1. Qual estação do ano é a sua preferida?",
+        alternativas: [
+            {
+                texto: "Verão",
+                afirmacao: [
+                    "Você seria o Verão. Que é uma estação muito quente e receptiva, é uma estação que a maior parte das pessoas gosta, por que tem mais disposição e também conseguem fazer muitos passeios e aproveitar ao máximo."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Inverno",
+                afirmacao: [
+                    "Você seria o Inverno. Que é uma estação mais discreta porém é considerada uma das estações do ano mais bonitas, pois ela traz muitas paisagens belissimas e as vezes até raras, por ser uma época mais fria podemos ver que algumas pessoas desaprovam."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "2. Qual emoção negativa mais te representa?",
+        alternativas: [
+            {
+                texto: "Medo",
+                afirmacao: [
+                    "Você seria a primavera. Pois é a estação do ano aonde as flores após se esconderem do frio, acabam desabrochando, sendo a abertura para outra estação do ano e uma das estações do ano mais bonitas que podemos observar, com varias paisagens repletas de flores.",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Tristeza",
+                afirmacao: [
+                    "Você seria o outono. É a estacão do ano aonde as flores se escondem para que comece o inverno é uma estação agradável pois nada é ao extremo, nem muito calor nem muito frio."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual a sua cor favorita?",
+        alternativas: [
+            {
+                texto: "Amarelo",
+                afirmacao: [
+                    "Você seria o outono. É a estacão do ano aonde as flores se escondem para que comece o inverno é uma estação agradável pois nada é ao extremo, nem muito calor nem muito frio."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: " Azul",
+                afirmacao: [
+                    "Você seria o Inverno. Que é uma estação mais discreta porém é considerada uma das estações do ano mais bonitas, pois ela traz muitas paisagens belissimas e as vezes até raras, por ser uma época mais fria podemos ver que algumas pessoas desaprovam."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual desses animais você mais gosta?",
+        alternativas: [
+            {
+                texto: "Leão",
+                afirmacao: [
+                    "Você seria o Verão. Que é uma estação muito quente e receptiva, é uma estação que a maior parte das pessoas gosta, por que tem mais disposição e também conseguem fazer muitos passeios e aproveitar ao máximo."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Cobra",
+                afirmacao: [
+                   "Você seria a primavera. Pois é a estação do ano aonde as flores após se esconderem do frio, acabam desabrochando, sendo a abertura para outra estação do ano e uma das estações do ano mais bonitas que podemos observar, com varias paisagens repletas de flores."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Você prefere morar num país?",
+        alternativas: [
+            {
+                texto: "Tropical",
+                afirmacao: [
+                    "Você seria o Verão. Que é uma estação muito quente e receptiva, é uma estação que a maior parte das pessoas gosta, por que tem mais disposição e também conseguem fazer muitos passeios e aproveitar ao máximo."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Polar",
+                afirmacao: [
+                    "Você seria o Inverno. Que é uma estação mais discreta porém é considerada uma das estações do ano mais bonitas, pois ela traz muitas paisagens belissimas e as vezes até raras, por ser uma época mais fria podemos ver que algumas pessoas desaprovam."
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Do que você não gosta?",
+        alternativas: [
+            {
+                texto: "Chuva",
+                afirmacao: [
+                    "Você seria o Verão. Que é uma estação muito quente e receptiva, é uma estação que a maior parte das pessoas gosta, por que tem mais disposição e também conseguem fazer muitos passeios e aproveitar ao máximo."
+                ],
+            },
+            {
+                texto: "Flores",
+                afirmacao: [
+                    "Você seria o outono. É a estacão do ano aonde as flores se escondem para que comece o inverno é uma estação agradável pois nada é ao extremo, nem muito calor nem muito frio."
+                ],
+                proxima: 9,
+            },
+        
+        ]
+    },
+    {
+        enunciado: "Se você tivesse que escolher morar para sempre em um dos extremos qual você escolheria?",
+        alternativas: [
+            {
+                texto: "Extremo frio",
+                afirmacao: [
+                    "Você seria o Inverno. Que é uma estação mais discreta porém é considerada uma das estações do ano mais bonitas, pois ela traz muitas paisagens belissimas e as vezes até raras, por ser uma época mais fria podemos ver que algumas pessoas desaprovam."
+                ],
+            },
+            {
+                texto: "Extremo calor",
+                afirmacao: [
+                    "Você seria o Verão. Que é uma estação muito quente e receptiva, é uma estação que a maior parte das pessoas gosta, por que tem mais disposição e também conseguem fazer muitos passeios e aproveitar ao máximo."
+                ],
+                proxima: 10,
+            },
+        ]
+    }
+]
